9d9e6461f7716c079056c0f771592d5fd9ad950d
Gabriella Lopes <gabriellalopes0630@gmail.com>
2021-06-30 22:37:00 -0300

    criando loja de games

---
+++ lojadegames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,65 @@
+package com.generation.lojagames.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nomeProduto;
+
+	@NotNull
+	private double precoProduto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public double getPrecoProduto() {
+		return precoProduto;
+	}
+
+	public void setPrecoProduto(double precoProduto) {
+		this.precoProduto = precoProduto;
+	}
+
+}
---
+++ lojadegames/src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.generation.lojagames.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.generation.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List<Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+}
